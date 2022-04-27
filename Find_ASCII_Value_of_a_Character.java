bca61da032d836842ae49fca57b68403f74576a9
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-27 19:00:32 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         char a;         int res;         Scanner sc= new Scanner(System.in);         a= sc.next().charAt(0);         res=(int)a;
            System.out.println(res);
        }
        
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        char a;
+        int res;
+        Scanner sc= new Scanner(System.in);
+        a= sc.next().charAt(0);
+        res=(int)a;
+        
+        System.out.println(res);
+    }
+    
+}
