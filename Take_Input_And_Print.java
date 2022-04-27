ac322e0653090e1ab93e504b21caaa904e687a02
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-27 18:51:48 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         String inp;         Scanner buf = new Scanner(System.in);         inp = buf.nextLine();         System.out.println(inp);
        }}

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        String inp;
+        Scanner buf = new Scanner(System.in);
+        inp = buf.nextLine();
+        System.out.println(inp);
+        
+    }}
