6ff578f7b7644171280ca776d9c79c582348060c
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-27 18:53:00 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int n,m,sum;         Scanner buf = new Scanner(System.in);         n = buf.nextInt();         m = buf.nextInt();         sum=n+m;         System.out.println(sum);
        }
    }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,m,sum;
+        Scanner buf = new Scanner(System.in);
+        n = buf.nextInt();
+        m = buf.nextInt();
+        sum=n+m;
+        System.out.println(sum);
+        
+    }
+}
