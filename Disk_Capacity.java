45106247c0234c7729ccc20209a48caf8240b627
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-27 18:56:59 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int t,s,b,res;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         s=sc.nextInt();         b=sc.nextInt();         res=t*s*b*512*2;         System.out.println(res);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        int t,s,b,res;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        s=sc.nextInt();
+        b=sc.nextInt();
+        res=t*s*b*512*2;
+        System.out.println(res);
+        
+    }
+}
