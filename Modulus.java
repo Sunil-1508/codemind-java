172835b92ff77c31e96d2e9d2fdeb593ffc0a6e1
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-27 18:50:22 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int n,m,mod;         Scanner buf = new Scanner(System.in);         n = buf.nextInt();         m = buf.nextInt();         mod=n%m;         System.out.println(mod);
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,m,mod;
+        Scanner buf = new Scanner(System.in);
+        n = buf.nextInt();
+        m = buf.nextInt();
+        mod=n%m;
+        System.out.println(mod);
+        
+    }
+}
+
