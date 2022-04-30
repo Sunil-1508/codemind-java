fb89e679352c73dd061e5c448d1e0bf3d4243625
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-30 11:25:57 +0530

    import java.util.*; import java.lang.Math; class main {     public static void main(String args[])     {        // double n,r,a;         //n=3.14;         int n,m,l;
            double s,a,b;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
             //m=sc.nextInt();
              //l=sc.nextInt();
            //a=n*r*r;
            //s=(float)(n+m+l)/2;
            //b=s*(s-n)*(s-m)*(s-l);//(s-n);//(s-m)(s-l);
            //a=Math.sqrt(b);
            //s=32+j;
            a=(float)n*1.8;
             s=(float)32+a;
            
            System.out.format("%.2f",s);
        }
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.Math;
+class main
+{
+    public static void main(String args[])
+    {
+       // double n,r,a;
+        //n=3.14;
+        int n,m,l;
+    
+        double s,a,b;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+         //m=sc.nextInt();
+          //l=sc.nextInt();
+        //a=n*r*r;
+        //s=(float)(n+m+l)/2;
+        //b=s*(s-n)*(s-m)*(s-l);//(s-n);//(s-m)(s-l);
+        //a=Math.sqrt(b);
+        //s=32+j;
+        a=(float)n*1.8;
+         s=(float)32+a;
+        
+        System.out.format("%.2f",s);
+    }
+}
