3b2d62f2c2e5201dd2df4fff43cbcfa0198bdcda
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-30 11:28:39 +0530

    import java.util.*; import java.lang.Math; class main {     public static void main(String args[])     {        // double n,r,a;         //n=3.14;         int n,m,l;
            double s,a,b;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
             
            a=n-32;
             s=(float)a/1.8;
            
            System.out.format("%.2f",s);
        }
    }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.Math;
+class main
+{
+    public static void main(String args[])
+    {
+       // double n,r,a;
+        //n=3.14;
+        int n,m,l;
+    
+        double s,a,b;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+         
+        a=n-32;
+         s=(float)a/1.8;
+        
+        System.out.format("%.2f",s);
+    }
+}
