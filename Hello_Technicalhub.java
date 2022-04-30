d0d841d67d398b7fbe388e33e0f14f059e098826
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-30 10:50:26 +0530

    import java.io.*; class sample{     public static void main(String args[])throws Exception{         int x,y;         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));         String s=br.readLine();         System.out.println("Hello Technicalhub");         System.out.println(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,10 @@
+ import java.io.*;
+class sample{
+    public static void main(String args[])throws Exception{
+        int x,y;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s);
+    }
+}
