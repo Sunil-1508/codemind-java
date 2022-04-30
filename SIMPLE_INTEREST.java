910dea04665d03c653b2a85e63cc7dd0b39e8af7
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-30 11:31:05 +0530

    import java.util.*; import java.lang.Math; class main {     public static void main(String args[])     {        // double n,r,a;         //n=3.14;         int n,m,l;         int s;         double a,b;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();          m=sc.nextInt();           l=sc.nextInt();
            s=(n*m*l)/100;
            
            System.out.print(s);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.Math;
+class main
+{
+    public static void main(String args[])
+    {
+       // double n,r,a;
+        //n=3.14;
+        int n,m,l;
+        int s;
+        double a,b;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+         m=sc.nextInt();
+          l=sc.nextInt();
+        
+        s=(n*m*l)/100;
+        
+        System.out.print(s);
+    }
+}
