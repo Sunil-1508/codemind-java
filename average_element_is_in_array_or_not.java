1f6e46b1a75e6fe4f5562258d25bc6b0a5cd5682
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-04-30 11:19:49 +0530

    import java.util.*; class main {     public static void main(String args[])     {         int n,k,l=0,count=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         //k=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();             count=count+a[i];         }         //k=sc.nextInt();         k=count/n;        for(int i=0;i<n;i++)     {      if(((a[i])==k))      {         //System.out.print("True");         l++;      }
        }
        if(l!=0)
        {
        //cout<<count;
        System.out.print("True");
        }
        else
        {
           System.out.print("False"); 
        }
          
           
           
            
        }
    }

---
+++ average_element_is_in_array_or_not.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        int n,k,l=0,count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        //k=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            count=count+a[i];
+        }
+        //k=sc.nextInt();
+        k=count/n;
+       for(int i=0;i<n;i++)
+    {
+     if(((a[i])==k))
+     {
+        //System.out.print("True");
+        l++;
+     }
+     
+    }
+    if(l!=0)
+    {
+    //cout<<count;
+    System.out.print("True");
+    }
+    else
+    {
+       System.out.print("False"); 
+    }
+      
+       
+       
+        
+    }
+}
